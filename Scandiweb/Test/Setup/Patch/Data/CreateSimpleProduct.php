7fea3a06a5ac3b90f0f75d90b0def98485b4ea47
mohamed.ayad@scandiweb.com <mohamed.ayad@scandiweb.com>
2023-03-09 09:33:58 +0200

    first commit

---
+++ Scandiweb/Test/Setup/Patch/Data/CreateSimpleProduct.php
@@ -0,0 +1,194 @@
+<?php
+
+/**
+ *
+ * @category Scandiweb
+ * @package Scandiweb_Test
+ * @author Mohammed Ayad <mohamed.ayad@scandiweb.com>
+ * @copyright Copyright (c) 2022 Scandiweb, Inc (https://scandiweb.com)
+ * @license http://opensource.org/licenses/OSL-3.0 The Open Software License 3.0 (OSL-3.0)
+ */
+
+declare(strict_types=1);
+
+namespace Scandiweb\Test\Setup\Patch\Data;
+
+use Magento\Catalog\Api\CategoryLinkManagementInterface;
+use Magento\Catalog\Api\Data\ProductInterfaceFactory;
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Catalog\Model\Product;
+use Magento\Catalog\Model\Product\Attribute\Source\Status;
+use Magento\Catalog\Model\Product\Type;
+use Magento\Catalog\Model\Product\Visibility;
+use Magento\Catalog\Model\ResourceModel\Category\CollectionFactory;
+use Magento\Eav\Setup\EavSetup;
+use Magento\Framework\App\State;
+use Magento\Framework\Exception\CouldNotSaveException;
+use Magento\Framework\Exception\InputException;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Validation\ValidationException;
+use Magento\InventoryApi\Api\Data\SourceItemInterface;
+use Magento\InventoryApi\Api\Data\SourceItemInterfaceFactory;
+use Magento\InventoryApi\Api\SourceItemsSaveInterface;
+use Magento\Store\Model\StoreManagerInterface;
+
+/**
+ * Create Migration product class
+ */
+class CreateSimpleProduct implements DataPatchInterface
+{
+    /**
+     * @var ProductInterfaceFactory
+     */
+    protected ProductInterfaceFactory $productInterfaceFactory;
+    /**
+     * @var CategoryLinkManagementInterface
+     */
+    protected CategoryLinkManagementInterface $categoryLink;
+
+    /**
+     * @var ProductRepositoryInterface
+     */
+    protected ProductRepositoryInterface $productRepository;
+
+    /**
+     * @var State
+     */
+    protected State $appState;
+
+    /**
+     * @var StoreManagerInterface
+     */
+    protected StoreManagerInterface $storeManager;
+
+    /**
+     * @var SourceItemInterfaceFactory
+     */
+    protected SourceItemInterfaceFactory $sourceItemFactory;
+
+    /**
+     * @var SourceItemsSaveInterface
+     */
+    protected SourceItemsSaveInterface $sourceItemsSaveInterface;
+
+    /**
+     * @var CollectionFactory
+     */
+    protected CollectionFactory $collectionFactory;
+
+    /**
+     * @var EavSetup
+     */
+    protected EavSetup $eavSetup;
+
+    /**
+     * @var array
+     */
+    protected array $sourceItems = [];
+
+    /**
+     * Migration patch constructor.
+     *
+     * @param ProductInterfaceFactory $productInterfaceFactory
+     * @param ProductRepositoryInterface $productRepository
+     * @param SourceItemInterfaceFactory $sourceItemFactory
+     * @param SourceItemsSaveInterface $sourceItemsSaveInterface
+     * @param State $appState
+     * @param StoreManagerInterface $storeManager
+     * @param EavSetup $eavSetup
+     * @param CategoryLinkManagementInterface $categoryLink
+     */
+    public function __construct(
+        CollectionFactory $collectionFactory,
+        ProductInterfaceFactory $productInterfaceFactory,
+        ProductRepositoryInterface $productRepository,
+        State $appState,
+        StoreManagerInterface $storeManager,
+        EavSetup $eavSetup,
+        SourceItemInterfaceFactory $sourceItemFactory,
+        SourceItemsSaveInterface $sourceItemsSaveInterface,
+        CategoryLinkManagementInterface $categoryLink
+    ) {
+        $this->appState = $appState;
+        $this->productInterfaceFactory = $productInterfaceFactory;
+        $this->productRepository = $productRepository;
+        $this->eavSetup = $eavSetup;
+        $this->storeManager = $storeManager;
+        $this->sourceItemFactory = $sourceItemFactory;
+        $this->sourceItemsSaveInterface = $sourceItemsSaveInterface;
+        $this->categoryLink = $categoryLink;
+        $this->collectionFactory = $collectionFactory;
+    }
+
+    /**
+     * Add new product
+     */
+    public function apply(): void
+    {
+        $this->appState->emulateAreaCode('adminhtml', [$this, 'execute']);
+    }
+
+    /**
+     * @throws CouldNotSaveException
+     * @throws InputException
+     * @throws LocalizedException
+     * @throws NoSuchEntityException
+     * @throws ValidationException
+     */
+    public function execute(): void
+    {
+
+        $product = $this->productInterfaceFactory->create();
+
+        if ($product->getIdBySku('shirt-test')) {
+            return;
+        }
+
+        $categoryTitles = ['Men', 'Women'];
+        $categoryIds = $this->collectionFactory->create()
+            ->addAttributeToFilter('name', ['in' => $categoryTitles])
+            ->getAllIds();
+
+        $attributeSetId = $this->eavSetup->getAttributeSetId(Product::ENTITY, 'Default');
+        $websiteIDs = [$this->storeManager->getStore()->getWebsiteId()];
+        $product->setTypeId(Type::TYPE_SIMPLE)
+            ->setWebsiteIds($websiteIDs)
+            ->setAttributeSetId($attributeSetId)
+            ->setName('T-Shirt')
+            ->setUrlKey('T-Shirt')
+            ->setSku('T-Shirt')
+            ->setPrice(15.99)
+            ->setVisibility(Visibility::VISIBILITY_BOTH)
+            ->setStatus(Status::STATUS_ENABLED)
+            ->setStockData(['use_config_manage_stock' => 1, 'is_qty_decimal' => 0, 'is_in_stock' => 1]);
+        $product = $this->productRepository->save($product);
+
+        $sourceItem = $this->sourceItemFactory->create();
+        $sourceItem->setSourceCode('default');
+        $sourceItem->setQuantity(10);
+        $sourceItem->setSku($product->getSku());
+        $sourceItem->setStatus(SourceItemInterface::STATUS_IN_STOCK);
+        $this->sourceItems[] = $sourceItem;
+
+        $this->categoryLink->assignProductToCategories($product->getSku(), $categoryIds);
+        $this->sourceItemsSaveInterface->execute($this->sourceItems);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public static function getDependencies(): array
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getAliases(): array
+    {
+        return [];
+    }
+}
